0f8faedaae42d589e907929c5fba936df59d5787
Jeff Hale <discdiver@users.noreply.github.com>
2025-01-28 13:51:47 -0500

    Update retries-delay.py

--- 102/retries-delay.py
+++ 102/retries-delay.py
@@ -2,7 +2,7 @@ import httpx
 from prefect import flow, task
 
 
-@task(retries=4, retry_delay_seconds=0.5)  # or [0.1, 0.5, 1, 2])
+@task(retries=4, retry_delay_seconds=2)  # or [1, 2, 5])
 def fetch_random_code():
     random_code = httpx.get("https://httpstat.us/Random/200,500", verify=False)
     if random_code.status_code >= 400:
