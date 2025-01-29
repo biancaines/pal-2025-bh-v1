646c9145f70143f87a3df1e47b332f535c582ae8
Jeff Hale <jeffmshale@gmail.com>
2025-01-29 14:08:36 -0500

    update example

--- 105/run-deployment.py
+++
@@ -1,5 +0,0 @@
-from prefect.deployments import run_deployment
-
-run_deployment(
-    name="pipeline/my-first-managed-deployment", parameters={"lat": 1, "lon": 2}
-)
--- 106/dask_example.py
+++ 106/dask_example.py
@@ -1,23 +1,24 @@
 from prefect import flow, task
 from prefect_dask.task_runners import DaskTaskRunner
+import asyncio
 
 
 @task
-def say_hello(name):
+async def say_hello(name):
     print(f"hello {name}")
 
 
 @task
-def say_goodbye(name):
+async def say_goodbye(name):
     print(f"goodbye {name}")
 
 
 @flow(task_runner=DaskTaskRunner())
-def greetings(names):
+async def greetings(names):
     for name in names:
         say_hello.submit(name)
         say_goodbye.submit(name)
 
 
 if __name__ == "__main__":
-    greetings(["arthur", "trillian", "ford", "marvin"])
+    asyncio.run(greetings(["arthur", "trillian", "ford", "marvin"]))
